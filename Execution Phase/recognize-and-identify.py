8583d76e6f3d496c70c3fdd6b00e433658a2bb18
4uSpock <ronmanto14@gmail.com>
2024-08-02 18:40:39 +0300

    Several QOL changes
    Added argument parsing to allow for better general use.
    
    Lowered threshold to accommodate the new model.
    
    used round() function in "Unknown" label return condition.
    
    Added a condition to the main loop, for it to close when the streaming window is closed.
    
    Changed the required number of frames to pass for the person to be changed.
    
    added an option to be used with images, so that one can run the model on an image and return the model's output.

--- Execution Phase/recognize-and-identify.py
+++ Execution Phase/recognize-and-identify.py
@@ -1,6 +1,7 @@
+# Complete code
 # Step 1: Loading trained model.
 # Step 2: Accessing Camera Stream.
-# Step 3: Face detection via FaceDetect NVIDIA model.
+# Step 3: Face detection via Facenet.
 # Step 4: Face recognition via trained model.
 # Note: Import Jetson lib. before the rest
 # If not - the glDisplay window won't open
@@ -17,12 +18,33 @@ from PIL import Image
 import numpy as np
 import time
 import collections
+import argparse
+import os
+
+#########################################
+# **       Parser Config             ** #
+#########################################
+
+parser = argparse.ArgumentParser(description="Locate objects in a live camera stream using an object detection DNN.", 
+                                 formatter_class=argparse.RawTextHelpFormatter)
+
+parser.add_argument("--input", type=str, default="/dev/video0", nargs='?', help="URI of the input stream")
+parser.add_argument("--save-dir", type=str, default="", help="directory to save cropped faces")
+parser.add_argument("--save-result", type=int, default=0, help="Would you like to save the image processing result?")
+
+try:
+    args = parser.parse_known_args()[0]
+except:
+    print("")
+    parser.print_help()
+    sys.exit(0)
 
 #########################################
 # ** Cameras and Display Config      ** #
 #########################################
 
-camera = jetson_utils.videoSource("/dev/video0")
+camera = jetson_utils.videoSource(args.input)
+#display = jetson_utils.videoOutput("webrtc://@:8554/outputStream", argv=['--headless'])
 display = jetson_utils.videoOutput()
 
 #########################################
@@ -36,7 +58,7 @@ transform = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
-dataset = datasets.ImageFolder('training-results', transform=transform)
+dataset = datasets.ImageFolder('/my-detection/Project/training-results', transform=transform)
 
 # Load the model
 model = models.resnet18(pretrained=True)
@@ -59,15 +81,14 @@ def preprocess_image(image, transform):
 
 
 # Function to predict the person in the image
-def predict(image, model, transform, device, threshold=0.7):
+def predict(image, model, transform, device, threshold=0.35):
     image_tensor = preprocess_image(image, transform)
     image_tensor = image_tensor.to(device)
     with torch.no_grad():
         outputs = model(image_tensor)
         probabilities = nn.Softmax(dim=1)(outputs)
         max_prob, preds = torch.max(probabilities, 1)
-
-        if max_prob.item() < threshold:
+        if round(max_prob.item(), 4) < threshold:
             return "Unknown"
 
     return dataset.classes[preds[0]]
@@ -80,13 +101,12 @@ print("Loading detection model...")
 faceDetector = jetson_inference.detectNet("facedetect", threshold = 0.8)
 print("Done.")
 
-# Initializing values
 recognized_faces = collections.deque(maxlen=5)
 pred_recognized_person = "Unknown"
 predicted_person_label = "Unknown"
 
-while True: #display.IsStreaming():
-	frame = camera.Capture()
+while display.IsStreaming():
+	frame = camera.Capture()	
 	face_detections = faceDetector.Detect(frame, overlay = "none")
 
 	if frame is None:
@@ -98,7 +118,6 @@ while True: #display.IsStreaming():
 	for i, detection in enumerate(face_detections):
 
 		# Aquiring Bounding Box Coordinates
-		# Doc: https://rawgit.com/dusty-nv/jetson-inference/dev/docs/html/python/jetson.inference.html
 		left = int(detection.Left)
 		top = int(detection.Top)
 		right = int(detection.Right)
@@ -108,28 +127,31 @@ while True: #display.IsStreaming():
 		extracted_face = cv_img[top:bottom, left:right]
 		
 		# Passing extracted face image to trained model
-		model_threshold = 0.9
+		model_threshold = 0.39
 		pred_recognized_person = predict(extracted_face, model, transform, device, model_threshold)
 		
 		recognized_faces.append(pred_recognized_person)
 		
 		# Update the label only if was recognized in the last 5 frames
-		if recognized_faces.count(pred_recognized_person) >= 3:
+		if recognized_faces.count(pred_recognized_person) >= 5:
 			predicted_person_label = pred_recognized_person		
 		
 		# Annotating image	
 		cv2.rectangle(cv_img, (left, top), (right, bottom), (46, 139, 87), 2)
-
-		cv2.putText(cv_img, predicted_person_label, (left, bottom - 20), cv2.FONT_HERSHEY_SIMPLEX, 2, (46, 139, 87), 2)
+		#predicted_person_label
+		cv2.putText(cv_img, pred_recognized_person, (left, bottom - 20), cv2.FONT_HERSHEY_SIMPLEX, 2, (46, 139, 87), 2)
 		print("Identified: ", predicted_person_label)
-
 	# Preparing image for rendering and display    
 	bgr_img = jetson_utils.cudaFromNumpy(cv_img, isBGR=True)
 	rgb_img = jetson_utils.cudaAllocMapped(width=bgr_img.width, height=bgr_img.height, format='rgb8')
 	jetson_utils.cudaConvertColor(bgr_img, rgb_img)
 	    
-	# Displaying image
-	display.Render(rgb_img)		
+			
+	
+	if args.save_result == 1:
+		cv2.imwrite(os.path.join(args.save_dir, f"result.jpg"), cv_img)
+	else:
+		# Displaying image
+		display.Render(rgb_img)
 
-	# Updating Status Bar
 	display.SetStatus("Facenet {:.0f} FPS | # faces :{:d}".format(faceDetector.GetNetworkFPS(), len(face_detections)))
